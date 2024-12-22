ef75f5a3ee62cdabbb0d7ed1daaaf8604c3251a6
rfrancotte <rodolphe.francotte@webedia-group.com>
2024-12-22 19:26:28 +0100

    gradle updated with libs.versions.toml

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -81,7 +81,7 @@ dependencies {
     // Serializer
     implementation ("com.squareup.retrofit2:converter-gson:2.9.0")
     // Lifecycle
-    val lifecycleVersion = "2.5.1"
+    val lifecycleVersion = "2.8.7"
     implementation("androidx.lifecycle:lifecycle-viewmodel-ktx:$lifecycleVersion")
     implementation("androidx.lifecycle:lifecycle-viewmodel-compose:$lifecycleVersion")
     implementation("androidx.lifecycle:lifecycle-livedata-ktx:$lifecycleVersion")
@@ -89,8 +89,8 @@ dependencies {
     implementation("androidx.lifecycle:lifecycle-viewmodel-savedstate:$lifecycleVersion")
     implementation("androidx.lifecycle:lifecycle-process:$lifecycleVersion")
     // Dagger Hilt
-    implementation("com.google.dagger:hilt-android:2.48")
-    kapt("com.google.dagger:hilt-android-compiler:2.48")
-    kapt("androidx.hilt:hilt-compiler:1.0.0")
+    implementation("com.google.dagger:hilt-android:2.52")
+    kapt("com.google.dagger:hilt-android-compiler:2.52")
+    kapt("androidx.hilt:hilt-compiler:1.2.0")
     coreLibraryDesugaring("com.android.tools:desugar_jdk_libs:2.0.0")
 }
